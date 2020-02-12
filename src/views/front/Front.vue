c9b26b70aef699f4945e6f16635069b5f9e77588
gretema <jeanma1995@gmail.com>
2020-02-12 11:32:29 +0800

    更改刪除商品 Modal 的模板

--- src/views/front/Front.vue
+++ src/views/front/Front.vue
@@ -7,32 +7,22 @@
     <Subscribe />
     <Footer />
     <!--刪除購物車商品 Modal-->
-    <div
-      class="modal fade"
-      id="delProductModal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content border-0">
-          <div class="modal-header bg-primary text-white">
-            <h5 class="modal-title" id="exampleModalLabel">
-              <span>刪除產品</span>
-            </h5>
+    <div class="modal fade" id="delProductModal" tabindex="-1" role="dialog"
+     aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+          <div class="modal-header bg-danger">
+            <h5 class="modal-title text-light" >刪除商品</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
-            是否自購物車刪除
-            <strong class="text-danger">{{ tempProduct.product.title }}</strong>
-            商品 (刪除後將無法恢復)。
+            是否自購物車刪除商品(刪除後將無法恢復)？
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">取消</button>
-            <button type="button" class="btn btn-primary" @click="removeCart">確認刪除</button>
+            <button type="button" class="btn btn-danger" @click="removeCart">確認刪除</button>
           </div>
         </div>
       </div>
